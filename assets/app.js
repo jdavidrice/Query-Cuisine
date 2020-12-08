45d3f325a67b5dc6cd3499efce63d04a89ab824a
Cris Franco <cris.franco219@gmail.com>
2020-12-08 15:00:05 -0500

    added jquery & script link

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+// Declare a variable to store the searched ingredient
+var ing = "",
+
+// Variable Declaration
+var searchIng = $("#search-ing");
+var searchButton = $("#search-button");
+var inglist = [];
+
+// Setup API key
+var APIkey = "2f3e7f513e714122ada8fe2295969951";
+
+// Function to get recipe
+function getRecipe(){
+    
+}
