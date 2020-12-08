66b67b5ff095bade085ce5d552d2962588271cd3
Cris Franco <cris.franco219@gmail.com>
2020-12-08 15:23:43 -0500

    added functions and AJAX call

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,26 @@ var searchButton = $("#search-button");
 var inglist = [];
 
 // Setup API key
-var APIkey = "2f3e7f513e714122ada8fe2295969951";
+const APIkey = "2f3e7f513e714122ada8fe2295969951";
 
-// Function to get recipe
-function getRecipe(){
+// Function to display recipe
+function displayRecipe(event){
+    event.preventDefault();
+    if(searchIng.val().trim()!==""){
+        ing = searchIng.val().trim();
+        getRecipe(ing);
+    }
     
+}
+
+// Function to get recipe using the ingredients
+function getRecipe(inglist){
+    // AJAX call
+    var queryURL =
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+    });
 }
