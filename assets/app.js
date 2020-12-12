6d05a46e546b05e283c0655e0ebd1feb2707c256
Danny Smooke <dana.smooke@gmail.com>
2020-12-12 15:19:30 -0500

    created ingredientTemp to loop through and then populate ingredientList with added items, would have gone further but reached spoon quota

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
 // Declare a variable to store the searched ingredient
-// var ingredient = $("#item1").val;
-var item1 = $("#item1").data-name();
-// var item2 = $("#item2");
-// var item3 = $("#item3");
-// var item4 = $("#item4");
-// var item5 = $("#item5");
+var item1 = $("#item1").val().trim();
+var item2 = $("#item2").val().trim();
+var item3 = $("#item3").val().trim();
+var item4 = $("#item4").val().trim();
+var item5 = $("#item5").val().trim();
 
-var ingredientList = [`${item1}, ${item2}, ${item3}, ${item4}, ${item5}`];
+var ingredientList = [];
+
 
 console.log(ingredientList);
 
@@ -15,28 +15,25 @@ var searchRecipe = $("#search-recipe").val();
 var addIngredient = $("#add-ing").val();
 
 
-console.log(ingredientList);
+// console.log(ingredientList);
 
 // Setup API key
-const APIKey = "2f3e7f513e714122ada8fe2295969951";
-const queryURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIKey}&ingredients=${ingredientList}&number=2`;
+var APIKey = "2f3e7f513e714122ada8fe2295969951";
+var queryURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIKey}&ingredients=${ingredientList}&number=2`;
 
-console.log(queryURL);
 
-//Function to add ingredient
-//function addIng(ingredient){
-    //ingredientList.push(ingredient)
+// console.log(queryURL);
 
-//}
-
-// Function to display recipe
-function displayRecipe(event){
-    event.preventDefault();
-    if(ingredientList.length !== 0){
-        getRecipe(ingredientList);
-    }
+// // Function to display recipe
+// function displayRecipe(event){
+//     event.preventDefault();
+//     if(ingredientList.length !== 0){
+//         getRecipe(ingredientList);
+//     }
     
-}
+// }
+
+
 
 // Function to get recipe using the ingredients ingredientList
 function getRecipe(){
@@ -56,9 +53,10 @@ function getRecipe(){
         $("#recipe1-title").text(response[0].title)
         console.log(response[0].title)
 
-        // var missedIngredients = [(response[0].missedIngredients[0].name), (response[0].missedIngredients[1].name)];
+        
+        // var missedIngredients = [(response[0].missedIngredients[0].name), (response[1].missedIngredients[0].name)];
 
-        // response[0].missedIngredients[0].metaInformation.unitShort  
+        
         
         // displaying missedIngredients
         $(".recipe1-ingredients").html("<li>" + 
@@ -68,16 +66,43 @@ function getRecipe(){
         // displaying usedIngredients
         $(".recipe1-ingredients").append("<li>" + response[0].usedIngredients[0].name + "</li>" + "</li>" + response[0].usedIngredients[1].name + "</li>");
 
-     
+        // display quantities of ingredients
+        // response[0].missedIngredients[0].metaInformation.unitShort  
+
     });
 }
 // calling getRecipe function
-getRecipe();
+// getRecipe();
 
 
 // Event listeners
 
-// search 
+// Submit button clicked, what happens each time
 $("#search-recipe").click(function(){
-    return getRecipe();
+    var item1 = $("#item1").val().trim();
+    var item2 = $("#item2").val().trim();
+    var item3 = $("#item3").val().trim();
+    var item4 = $("#item4").val().trim();
+    var item5 = $("#item5").val().trim();
+    var ingredientTemp = [item1, item2, item3, item4, item5];
+    ingredientList = [];
+
+    // loop through ingredientTemp, if ingredient entered add to ingredientList array
+    for (i=0; i<=4; i++) {
+        if(ingredientTemp[i] != ""){
+        ingredientList.push(ingredientTemp[i])
+        }
+    }
+    
+    // ingredientList = [item1, item2, item3, item4, item5];
+   
+    queryURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIKey}&ingredients=${ingredientList}&number=2`;
+
+    console.log(queryURL);
+    console.log(ingredientList); 
+
+    // getRecipe each time click submit button
+    getRecipe();
 });
+
+
