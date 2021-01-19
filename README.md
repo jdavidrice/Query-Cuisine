ff2950ff8b99c6616384b5c63e728d2a74e8588d
Jeremy Rice <jdavidrice@gmail.com>
2021-01-18 23:30:30 -0500

    Fix style issues with Codacy

--- README.md
+++ README.md
@@ -8,38 +8,36 @@ A simple food app that allows users to search for recipes based on ingredients t
 
 ## Table of Contents
 
-* [Description](#Description)
-* [Table of Contents](#Table-of-Contents)
-* [Photos](#Photos)
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [Contributing](#Contributing)
-
+*   [Description](#Description)
+*   [Table of Contents](#Table-of-Contents)
+*   [Photos](#Photos)
+*   [Installation](#Installation)
+*   [Usage](#Usage)
+*   [Contributing](#Contributing)
 
 ## Photos
 
 ![Screenshot 1 of published website](assets/images/query_cuisine_pic_1.png)
 ![Screenshot 2 of published website](assets/images/query_cuisine_pic_2.png)
 
-* [Return to Top](#Query-Cuisine)
+*   [Return to Top](#Query-Cuisine)
 
 ## Installation
 
 There is nothing to install to utilize this web app. 
 
-* [Return to Top](#Query-Cuisine)
+*   [Return to Top](#Query-Cuisine)
 
 ## Usage 
 
 Use this browser-based food app to find delicious recipes for the food that you already have in your fridge!
 After navigating to the correct web address, enter up to five ingredients into the text areas under the "Choose your ingredients" heading, then click on "submit." Your recipe suggestions will appear under the "Suggested Recipes" heading. 
 
-* [Return to Top](#Query-Cuisine)
+*   [Return to Top](#Query-Cuisine)
 
 
 ## Contributing
 
 Paul Jakubowski, Jeremy Rice, Dana Smooke, Cris Franco
 
-* [Return to Top](#Query-Cuisine)
-
+*   [Return to Top](#Query-Cuisine)
--- assets/app.js
+++ assets/app.js
@@ -29,40 +29,40 @@ function getRecipe() {
     }).then(function (response) {
 
         // log the queryURL 
-        console.log(response);
+        // console.log(response);
 
         // for loop to display recipe ingredients, img, title
         for (var i = 0; i < response.length; i++) {
-            var recipeDiv = $('<div class="recipe">')
+            var recipeDiv = $("<div class='recipe'>");
 
-            recipeDiv.append("<h4>" + response[i].title + "</h4>")
+            recipeDiv.append(`<h4>${response[i].title}</h4>`);
 
-            recipeDiv.append('<img src="' + response[i].image + '" />')
+            recipeDiv.append(`<img src='${response[i].image}'/>`);
 
             // for loop to get ingredients from recipe1 ...?
-            recipeDiv.append("<ul>")
+            recipeDiv.append("<ul>");
             for (var k = 0; k < response[i].usedIngredients.length; k++) {
 
                 // console.log(i);
                 // console.log(k);
 
                 // displaying usedIngredients
-                recipeDiv.append("<li>" + response[i].usedIngredients[k].originalString + "</li>")
+                recipeDiv.append(`<li>${response[i].usedIngredients[k].originalString}</li>`);
             }
 
             for (var k = 0; k < response[i].missedIngredients.length; k++) {
 
                 // displaying missedIngredients
-                recipeDiv.append("<li>" + response[i].missedIngredients[k].originalString + "</li>")
+                recipeDiv.append(`<li>${response[i].missedIngredients[k].originalString}</li>`);
             }
 
-            recipeDiv.append("</ul>")
+            recipeDiv.append("</ul>");
 
-            recipeDiv.append("<h4>Instructions</h4>")
+            recipeDiv.append("<h4>Instructions</h4>");
 
             getInstructions(response[i].id, recipeDiv);
 
-            $('#recipeCards').append(recipeDiv);
+            $("#recipeCards").append(recipeDiv);
         }
     });
 }
@@ -70,7 +70,7 @@ function getRecipe() {
 
 function getInstructions(recipeId, recipeDiv) {
     var how2URL = `https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions?apiKey=${APIKey}`;
-    console.log(how2URL);
+    // console.log(how2URL);
 
     $.ajax({
         url: how2URL,
@@ -79,21 +79,21 @@ function getInstructions(recipeId, recipeDiv) {
     }).then(function (instructions) {
 
 
-        var olEl = $("<ol id='problem'>")
+        var olEl = $("<ol id='problem'>");
 
 
         for (var j = 0; j < instructions[0].steps.length; j++) {
 
             // console.log(j);
 
-            var problem = $("<li>")
+            var problem = $("<li>");
             problem.text(instructions[0].steps[j].step);
             olEl.append(problem);
         }
 
         recipeDiv.append(olEl);
 
-    })
+    });
 }; // end of getInstructions function
 
 
@@ -108,8 +108,8 @@ $("#search-recipe").click(function () {
 
     // loop through ingredientTemp, if ingredient entered add to ingredientList array
     for (i = 0; i <= 4; i++) {
-        if (ingredientTemp[i] != "") {
-            ingredientList.push(ingredientTemp[i])
+        if (ingredientTemp[i] !== "") {
+            ingredientList.push(ingredientTemp[i]);
         }
     }
 
